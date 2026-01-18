d7a28f3d6a4cce49209888ff779801b6ca097ea5
Kevin Aud <kevinaud@gmail.com>
2026-01-18 18:13:31 +0000

    chore: reformat generated proto files (prettier/ruff)

--- packages/adk-sim-protos-ts/src/google/type/latlng_pb.ts
+++ packages/adk-sim-protos-ts/src/google/type/latlng_pb.ts
@@ -16,15 +16,18 @@
 // @generated from file google/type/latlng.proto (package google.type, syntax proto3)
 /* eslint-disable */
 
-import type { GenFile, GenMessage } from "@bufbuild/protobuf/codegenv2";
-import { fileDesc, messageDesc } from "@bufbuild/protobuf/codegenv2";
-import type { Message } from "@bufbuild/protobuf";
+import type { GenFile, GenMessage } from '@bufbuild/protobuf/codegenv2';
+import { fileDesc, messageDesc } from '@bufbuild/protobuf/codegenv2';
+import type { Message } from '@bufbuild/protobuf';
 
 /**
  * Describes the file google/type/latlng.proto.
  */
-export const file_google_type_latlng: GenFile = /*@__PURE__*/
-  fileDesc("Chhnb29nbGUvdHlwZS9sYXRsbmcucHJvdG8SC2dvb2dsZS50eXBlIi0KBkxhdExuZxIQCghsYXRpdHVkZRgBIAEoARIRCglsb25naXR1ZGUYAiABKAFCYwoPY29tLmdvb2dsZS50eXBlQgtMYXRMbmdQcm90b1ABWjhnb29nbGUuZ29sYW5nLm9yZy9nZW5wcm90by9nb29nbGVhcGlzL3R5cGUvbGF0bG5nO2xhdGxuZ/gBAaICA0dUUGIGcHJvdG8z");
+export const file_google_type_latlng: GenFile =
+  /*@__PURE__*/
+  fileDesc(
+    'Chhnb29nbGUvdHlwZS9sYXRsbmcucHJvdG8SC2dvb2dsZS50eXBlIi0KBkxhdExuZxIQCghsYXRpdHVkZRgBIAEoARIRCglsb25naXR1ZGUYAiABKAFCYwoPY29tLmdvb2dsZS50eXBlQgtMYXRMbmdQcm90b1ABWjhnb29nbGUuZ29sYW5nLm9yZy9nZW5wcm90by9nb29nbGVhcGlzL3R5cGUvbGF0bG5nO2xhdGxuZ/gBAaICA0dUUGIGcHJvdG8z',
+  );
 
 /**
  * An object that represents a latitude/longitude pair. This is expressed as a
@@ -35,7 +38,7 @@ export const file_google_type_latlng: GenFile = /*@__PURE__*/
  *
  * @generated from message google.type.LatLng
  */
-export type LatLng = Message<"google.type.LatLng"> & {
+export type LatLng = Message<'google.type.LatLng'> & {
   /**
    * The latitude in degrees. It must be in the range [-90.0, +90.0].
    *
@@ -55,5 +58,6 @@ export type LatLng = Message<"google.type.LatLng"> & {
  * Describes the message google.type.LatLng.
  * Use `create(LatLngSchema)` to create a new message.
  */
-export const LatLngSchema: GenMessage<LatLng> = /*@__PURE__*/
+export const LatLngSchema: GenMessage<LatLng> =
+  /*@__PURE__*/
   messageDesc(file_google_type_latlng, 0);
